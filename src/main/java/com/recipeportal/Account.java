970944a0dee278620313a49e581bf5ce7b96246d
dm255657 <255657@edu.p.lodz.pl>
2026-06-18 11:20:13 +0200

    feat(RP-5): implement password change logic with verification

--- src/main/java/com/recipeportal/Account.java
+++ src/main/java/com/recipeportal/Account.java
@@ -10,14 +10,15 @@ public class Account {
         this.password = password;
     }
 
-    public String getProfilePictureUrl() {
-        return profilePictureUrl;
+    public String getPassword() {
+        return password;
     }
 
-    public void setProfilePictureUrl(String profilePictureUrl) {
-        this.profilePictureUrl = profilePictureUrl;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() { return email; }
-    public String getPassword() { return password; }
+    public String getProfilePictureUrl() { return profilePictureUrl; }
+    public void setProfilePictureUrl(String url) { this.profilePictureUrl = url; }
 }
