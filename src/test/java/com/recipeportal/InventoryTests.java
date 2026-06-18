dda55fabe45f474190ac78a84e63de355f32be8b
Roman Voitovych <258502@edu.p.lodz.pl>
2026-06-18 11:02:24 +0200

    Changed Class name in inventory tests

--- src/test/java/com/recipeportal/InventoryTests.java
+++ src/test/java/com/recipeportal/InventoryTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class InventoryTest {
+public class InventoryTests {
 
     @Test
     void shouldAddNewIngredientToInventory() {
