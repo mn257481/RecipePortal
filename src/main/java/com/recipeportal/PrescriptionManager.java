ce073848e4d9c42a9030ecac48e8597df1ce2de5
Roman Voitovych <258502@edu.p.lodz.pl>
2026-06-18 11:16:20 +0200

    Added prescription manager class

---
+++ src/main/java/com/recipeportal/PrescriptionManager.java
@@ -0,0 +1,30 @@
+package com.recipeportal;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrescriptionManager {
+
+    private final Map<String, Prescription> prescriptions = new HashMap<>();
+
+    public void createPrescription(String name) {
+        if (prescriptions.containsKey(name)) {
+            throw new IllegalArgumentException("Prescription already exists.");
+        }
+
+        prescriptions.put(name, new Prescription(name));
+    }
+
+    public Prescription getPrescription(String name) {
+        return prescriptions.get(name);
+    }
+
+    public Collection<Prescription> getAllPrescriptions() {
+        return prescriptions.values();
+    }
+
+    public boolean containsPrescription(String name) {
+        return prescriptions.containsKey(name);
+    }
+}
